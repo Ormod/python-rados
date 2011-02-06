0bcc024dd1292dbb065b01bbfeecbdb140d5bfc6
Hannu Valtonen <hannu.valtonen@ormod.com>
2011-02-06 22:19:57 +0200

    Add support for xattrs and stat. Object stat works but pool_stat seems to segfault.

--- rados/rados.py
+++ rados/rados.py
@@ -1,7 +1,8 @@
 """librados Python ctypes wrapper
 @author: Hannu Valtonen <hannu.valtonen@ormod.com>
 """
-from ctypes import CDLL, c_char_p, c_size_t, c_void_p, create_string_buffer, byref
+from ctypes import CDLL, c_char_p, c_size_t, c_void_p, create_string_buffer, byref, Structure, c_uint64, pointer
+import time
 
 class RadosError(Exception):
     pass
@@ -14,6 +15,19 @@ class WriteError(Exception):
 
 class IncompleteWriteError(Exception):
     pass
+
+class rados_pool_stat_t(Structure):
+    _fields_ = [("num_bytes", c_uint64),
+                ("num_kb", c_uint64),
+                ("num_objects", c_uint64),
+                ("num_object_clones", c_uint64),
+                ("num_object_copies", c_uint64),
+                ("num_objects_missing_on_primary", c_uint64),
+                ("num_objects_unfound", c_uint64),
+                ("num_rd", c_uint64),
+                ("num_rd_kb", c_uint64),
+                ("num_wr", c_uint64),
+                ("num_wr_kb", c_uint64)]
 
 def object_deleted(method):
     def wrapper(self, *args, **kwargs):
@@ -47,6 +61,14 @@ class Rados(object):
         if ret < 0:
             raise RadosError("pool couldn't be deleted")
 
+    def get_pool(self, pool_name):
+        try:
+            pool = self.open_pool(pool_name)
+        except RadosError:
+            self.create_pool(pool_name)
+            pool = self.open_pool(pool_name)
+        return pool
+        
     def open_pool(self, pool_name):
         pool = c_void_p()
         ret = self.librados.rados_open_pool(c_char_p(pool_name), byref(pool))
@@ -92,37 +114,97 @@ class RadosPool(object):
         return ret_buf.value
 
     @object_deleted
+    def get_stats(self):
+        stats = rados_pool_stat_t()
+        #problem with this, causes a segfault when called
+        ret = self.librados.rados_stat_pool(self.pool, byref(stats))
+        if ret < 0:
+            raise RadosError("Couldn't get stats from pool")
+        return stats
+
+    @object_deleted
     def remove_object(self, key):
         ret = self.librados.rados_remove(self.pool, c_char_p(key))
         if ret < 0:
             raise RadosError("Delete failure")
         return True
 
+    @object_deleted
+    def stat(self, key):
+        """Stat object, returns, size/timestamp"""
+        psize = c_uint64()
+        pmtime = c_uint64()
+
+        ret = self.librados.rados_stat(self.pool, c_char_p(key), pointer(psize), pointer(pmtime))
+        if ret < 0:
+            raise RadosError("Failed to stat %r" % key)
+        return psize.value, time.localtime(pmtime.value)
+
+    @object_deleted
+    def get_xattr(self, key, xattr_name):
+        ret_length = 4096
+        ret_buf = create_string_buffer(ret_length)
+        ret = self.librados.rados_getxattr(self.pool, c_char_p(key), c_char_p(xattr_name), ret_buf, c_size_t(ret_length))
+        if ret < 0:
+            raise RadosError("Failed to get xattr %r" % xattr_name)
+        return ret_buf.value
+
+    @object_deleted
+    def set_xattr(self, key, xattr_name, xattr_value):
+        ret = self.librados.rados_setxattr(self.pool, c_char_p(key), c_char_p(xattr_name), c_char_p(xattr_value), c_size_t(len(xattr_value)))
+        if ret < 0:
+            raise RadosError("Failed to set xattr %r" % xattr_name)
+        return True
+
+    @object_deleted
+    def rm_xattr(self, key, xattr_name):
+        ret = self.librados.rados_rmxattr(self.pool, c_char_p(key), c_char_p(xattr_name))
+        if ret < 0:
+            raise RadosError("Failed to delete key %r xattr %r" % (key, xattr_name))
+        return True
+
 class RadosObject(object):
     """Rados object wrapper, makes the object look like a file"""
     def __init__(self, pool, key):
-        self.oid = key
+        self.key = key
         self.pool = pool
         self.offset = 0
         self.deleted = False
 
     @object_deleted
     def read(self, length = 1024*1024):
-        ret = self.pool.read(self.oid, self.offset, length)
+        ret = self.pool.read(self.key, self.offset, length)
         self.offset += len(ret)
         return ret
 
     @object_deleted
     def write(self, string_to_write):
-        ret = self.pool.write(self.oid, string_to_write, self.offset)
+        ret = self.pool.write(self.key, string_to_write, self.offset)
         self.offset += ret
         return ret
 
     @object_deleted
     def remove(self):
-        self.pool.remove_object(self.oid)
+        self.pool.remove_object(self.key)
         self.deleted = True
 
     @object_deleted
+    def stat(self):
+        return self.pool.stat(self.key)
+    
+    @object_deleted
     def seek(self, position):
         self.offset = position
+    
+    @object_deleted
+    def get_xattr(self, xattr_name):
+        return self.pool.get_xattr(self.key, xattr_name)
+
+    @object_deleted
+    def set_xattr(self, xattr_name, xattr_value):
+        return self.pool.set_xattr(self.key, xattr_name, xattr_value)
+
+    @object_deleted
+    def rm_xattr(self, xattr_name):
+        return self.pool.rm_xattr(self.key, xattr_name)
+
